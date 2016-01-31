d3aa6ae2db6eebfca858d613700fe15f5ca88d5e
ireineck <ireineck@student.cvtc.edu>
2016-01-31 10:23:55 -0600

    Initial Commit

---
+++ Hotel Bill Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hotel Bill Calculator
+//
+//  Created by Ian Reineck on 1/31/16.
+//  Copyright © 2016 Ian Reineck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
